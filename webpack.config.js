5f8ad17f7c399a3371e82e44960060977090249e
Behind The Math <info@behindthemath.io>
2017-10-03 01:10:48 -0400

    Add Webpack for minified builds

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    context: __dirname,
+    entry: {
+        "kosher-zmanim": "./src/kosher-zmanim.ts",
+        "kosher-zmanim.min": "./src/kosher-zmanim.ts"
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "./dist"),
+        libraryTarget: "umd",
+        library: "KosherZmanim",
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true,
+                            logInfoToStdOut: true,
+                            compilerOptions: {
+                                target: "es5",
+                                module: "es6"
+                            },
+                            configFile: "src/tsconfig.json"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    plugins: [
+        new UglifyJSPlugin({
+                sourceMap: true,
+                include: /\.min\.js$/,
+                uglifyOptions: {
+                    mangle: {
+                        keep_fnames: true
+                    }
+                }
+            }
+        )
+    ]
+};
+
