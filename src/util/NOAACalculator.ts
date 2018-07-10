BehindTheMath/KosherZmanim

commit 308f71d7ef64fde6a62c59bb7606bb4ce8b68091

Change usages of Math.floor() to Math.trunc
Math.trunc accurately mirrors a cast to int in Java

src/util/NOAACalculator.ts
@@ -96,7 +96,7 @@ export default class NOAACalculator extends AstronomicalCalculator {
         const a: number = year / 100;
         const b: number = 2 - a + a / 4;
 
-        return Math.floor(365.25 * (year + 4716)) + Math.floor(30.6001 * (month + 1)) + day + b - 1524.5;
+        return Math.trunc(365.25 * (year + 4716)) + Math.trunc(30.6001 * (month + 1)) + day + b - 1524.5;
     }
 
     /**

src/util/SunTimesCalculator.ts
@@ -155,8 +155,8 @@ export default class SunTimesCalculator extends AstronomicalCalculator {
         const a: number = 0.91764 * SunTimesCalculator.tanDeg(sunTrueLongitude);
         let ra: number = 360.0 / (2.0 * Math.PI) * Math.atan(a);
 
-        const lQuadrant: number = Math.floor(sunTrueLongitude / 90.0) * 90.0;
-        const raQuadrant: number = Math.floor(ra / 90.0) * 90.0;
+        const lQuadrant: number = Math.trunc(sunTrueLongitude / 90) * 90;
+        const raQuadrant: number = Math.trunc(ra / 90) * 90;
         ra = ra + (lQuadrant - raQuadrant);
 
         return ra / SunTimesCalculator.DEG_PER_HOUR; // convert to hours

src/util/ZmanimCalculator.ts
@@ -66,8 +66,8 @@ export default class ZmanimCalculator extends AstronomicalCalculator {
         }
 
         // step 5b: right ascension value needs to be in the same quadrant as L
-        const lQuadrant: number = Math.floor(l / 90) * 90;
-        const raQuadrant: number = Math.floor(RA / 90) * 90;
+        const lQuadrant: number = Math.trunc(l / 90) * 90;
+        const raQuadrant: number = Math.trunc(RA / 90) * 90;
         RA = RA + (lQuadrant - raQuadrant);
 
         // step 5c: right ascension value needs to be converted into hours
@@ -146,8 +146,8 @@ export default class ZmanimCalculator extends AstronomicalCalculator {
         }
 
         // step 5b: right ascension value needs to be in the same quadrant as L
-        const Lquadrant: number = Math.floor(L / 90) * 90;
-        const RAquadrant: number = Math.floor(RA / 90) * 90;
+        const Lquadrant: number = Math.trunc(L / 90) * 90;
+        const RAquadrant: number = Math.trunc(RA / 90) * 90;
         RA = RA + (Lquadrant - RAquadrant);
 
         // step 5c: right ascension value needs to be converted into hours

src/AstronomicalCalendar.ts
@@ -579,11 +579,11 @@ export default class AstronomicalCalendar {
             cal.add(Calendar.DAY_OF_MONTH, 1);
         }
 
-        const hours: number = Math.floor(calculatedTime); // retain only the hours
+        const hours: number = Math.trunc(calculatedTime); // retain only the hours
         calculatedTime -= hours;
-        const minutes: number = Math.floor(calculatedTime *= 60); // retain only the minutes
+        const minutes: number = Math.trunc(calculatedTime *= 60); // retain only the minutes
         calculatedTime -= minutes;
-        const seconds: number = Math.floor(calculatedTime *= 60); // retain only the seconds
+        const seconds: number = Math.trunc(calculatedTime *= 60); // retain only the seconds
         calculatedTime -= seconds; // remaining milliseconds
 
         cal.set(Calendar.HOUR_OF_DAY, hours);

src/hebrewcalendar/HebrewDateFormatter.ts
@@ -570,12 +570,12 @@ export default class HebrewDateFormatter {
 
         let days: number = adjustedChalakim / DAY_CHALAKIM;
         adjustedChalakim = adjustedChalakim - (days * DAY_CHALAKIM);
-        const hours: number = Math.floor(adjustedChalakim / HOUR_CHALAKIM);
+        const hours: number = Math.trunc(adjustedChalakim / HOUR_CHALAKIM);
         if (hours >= 6) {
             days += 1;
         }
         adjustedChalakim = adjustedChalakim - (hours * HOUR_CHALAKIM);
-        const minutes: number = Math.floor(adjustedChalakim / MINUTE_CHALAKIM);
+        const minutes: number = Math.trunc(adjustedChalakim / MINUTE_CHALAKIM);
         adjustedChalakim = adjustedChalakim - minutes * MINUTE_CHALAKIM;
         return "Day: " + days % 7 + " hours: " + hours + ", minutes " + minutes + ", chalakim: " + adjustedChalakim;
     }

src/hebrewcalendar/JewishCalendar.ts
@@ -500,10 +500,10 @@ export default class JewishCalendar extends JewishDate {
         //cal.clear();
         const moladSeconds: number = molad.getMoladChalakim() * 10 / 3;
         cal.set(molad.getGregorianYear(), molad.getGregorianMonth(), molad.getGregorianDayOfMonth(),
-                molad.getMoladHours(), molad.getMoladMinutes(), Math.floor(moladSeconds));
-        cal.set(Calendar.MILLISECOND, Math.floor(1000 * (moladSeconds - Math.floor(moladSeconds))));
+                molad.getMoladHours(), molad.getMoladMinutes(), Math.trunc(moladSeconds));
+        cal.set(Calendar.MILLISECOND, Math.trunc(1000 * (moladSeconds - Math.trunc(moladSeconds))));
         // subtract local time difference of 20.94 minutes (20 minutes and 56.496 seconds) to get to Standard time
-        cal.add(Calendar.MILLISECOND, -1 * Math.floor(geo.getLocalMeanTimeOffset()));
+        cal.add(Calendar.MILLISECOND, -1 * Math.trunc(geo.getLocalMeanTimeOffset()));
         return cal.getTime();
     }
 

src/hebrewcalendar/JewishDate.ts
@@ -413,8 +413,8 @@ export default class JewishDate /*implements Comparable<JewishDate>, Cloneable*/
      */
     public static getJewishCalendarElapsedDays(year: number): number {
         const chalakimSince: number = JewishDate.getChalakimSinceMoladTohu(year, JewishDate.TISHREI);
-        const moladDay: number = Math.floor(chalakimSince / JewishDate.CHALAKIM_PER_DAY);
-        const moladParts: number = Math.floor(chalakimSince - moladDay * JewishDate.CHALAKIM_PER_DAY);
+        const moladDay: number = Math.trunc(chalakimSince / JewishDate.CHALAKIM_PER_DAY);
+        const moladParts: number = Math.trunc(chalakimSince - moladDay * JewishDate.CHALAKIM_PER_DAY);
         // delay Rosh Hashana for the 4 dechiyos
         return JewishDate.addDechiyos(year, moladDay, moladParts);
     }
@@ -844,7 +844,7 @@ export default class JewishDate /*implements Comparable<JewishDate>, Cloneable*/
      * @return the number of days from the Jewish epoch
      */
     private static moladToAbsDate(chalakim: number): number {
-        return Math.floor(chalakim / JewishDate.CHALAKIM_PER_DAY) + JewishDate.JEWISH_EPOCH;
+        return Math.trunc(chalakim / JewishDate.CHALAKIM_PER_DAY) + JewishDate.JEWISH_EPOCH;
     }
 
     /**
@@ -859,8 +859,8 @@ export default class JewishDate /*implements Comparable<JewishDate>, Cloneable*/
     public JewishDate(molad: number) {
         this.absDateToDate(JewishDate.moladToAbsDate(molad));
         // long chalakimSince = getChalakimSinceMoladTohu(year, JewishDate.TISHREI);// tishrei
-        const conjunctionDay: number = Math.floor(molad / JewishDate.CHALAKIM_PER_DAY);
-        const conjunctionParts: number = Math.floor(molad - conjunctionDay * JewishDate.CHALAKIM_PER_DAY);
+        const conjunctionDay: number = Math.trunc(molad / JewishDate.CHALAKIM_PER_DAY);
+        const conjunctionParts: number = Math.trunc(molad - conjunctionDay * JewishDate.CHALAKIM_PER_DAY);
         this.setMoladTime(conjunctionParts);
     }
 */
@@ -928,8 +928,8 @@ export default class JewishDate /*implements Comparable<JewishDate>, Cloneable*/
             const molad = jewishYearOrCalendarOrDateOrMolad as number;
             this.absDateToDate(JewishDate.moladToAbsDate(molad));
             // long chalakimSince = getChalakimSinceMoladTohu(year, JewishDate.TISHREI);// tishrei
-            const conjunctionDay: number = Math.floor(molad / JewishDate.CHALAKIM_PER_DAY);
-            const conjunctionParts: number = Math.floor(molad - conjunctionDay * JewishDate.CHALAKIM_PER_DAY);
+            const conjunctionDay: number = Math.trunc(molad / JewishDate.CHALAKIM_PER_DAY);
+            const conjunctionParts: number = Math.trunc(molad - conjunctionDay * JewishDate.CHALAKIM_PER_DAY);
             this.setMoladTime(conjunctionParts);
         }
     }

src/hebrewcalendar/YomiCalculator.ts
@@ -116,6 +116,6 @@ export default class YomiCalculator {
         }
         const a: number = year / 100;
         const b: number = 2 - a + a / 4;
-        return Math.floor(365.25 * (year + 4716) + Math.floor(30.6001 * (month + 1)) + day + b - 1524.5);
+        return Math.trunc(365.25 * (year + 4716) + Math.trunc(30.6001 * (month + 1)) + day + b - 1524.5);
     }
 }

commit 503d2aa7d67c614c5da0a133d5e2a8f7622bf4b8

Fixes a bug caused by using Math.trunc instead of Math.floor
This was caused by a regression introduced by 308f71d. The JS
equivalent of Java's cast to int is indeed Math.trunc, but not all
the instances in that commit were ints.

When dealing with negative numbers, Math.trunc and Math.floor
return different results.

src/util/NOAACalculator.ts
@@ -97,7 +97,7 @@ export default class NOAACalculator extends AstronomicalCalculator {
         const a: number = year / 100;
         const b: number = 2 - a + a / 4;
 
-        return Math.trunc(365.25 * (year + 4716)) + Math.trunc(30.6001 * (month + 1)) + day + b - 1524.5;
+        return Math.floor(365.25 * (year + 4716)) + Math.floor(30.6001 * (month + 1)) + day + b - 1524.5;
     }
 
     /**

src/util/SunTimesCalculator.ts
@@ -146,8 +146,8 @@ export default class SunTimesCalculator extends AstronomicalCalculator {
         const a: number = 0.91764 * SunTimesCalculator.tanDeg(sunTrueLongitude);
         let ra: number = 360 / (2 * Math.PI) * Math.atan(a);
 
-        const lQuadrant: number = Math.trunc(sunTrueLongitude / 90) * 90;
-        const raQuadrant: number = Math.trunc(ra / 90) * 90;
+        const lQuadrant: number = Math.floor(sunTrueLongitude / 90) * 90;
+        const raQuadrant: number = Math.floor(ra / 90) * 90;
         ra = ra + (lQuadrant - raQuadrant);
 
         return ra / SunTimesCalculator.DEG_PER_HOUR; // convert to hours

src/util/ZmanimCalculator.ts
@@ -68,8 +68,8 @@ export default class ZmanimCalculator extends AstronomicalCalculator {
         }
 
         // step 5b: right ascension value needs to be in the same quadrant as L
-        const lQuadrant: number = Math.trunc(l / 90) * 90;
-        const raQuadrant: number = Math.trunc(RA / 90) * 90;
+        const lQuadrant: number = Math.floor(l / 90) * 90;
+        const raQuadrant: number = Math.floor(RA / 90) * 90;
         RA = RA + (lQuadrant - raQuadrant);
 
         // step 5c: right ascension value needs to be converted into hours
@@ -148,8 +148,8 @@ export default class ZmanimCalculator extends AstronomicalCalculator {
         }
 
         // step 5b: right ascension value needs to be in the same quadrant as L
-        const Lquadrant: number = Math.trunc(L / 90) * 90;
-        const RAquadrant: number = Math.trunc(RA / 90) * 90;
+        const Lquadrant: number = Math.floor(L / 90) * 90;
+        const RAquadrant: number = Math.floor(RA / 90) * 90;
         RA = RA + (Lquadrant - RAquadrant);
 
         // step 5c: right ascension value needs to be converted into hours