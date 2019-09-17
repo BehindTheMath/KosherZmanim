b100f9ca517ad985ac9c8db928aa3e3df7dfc60c
Behind The Math <info@behindthemath.io>
2019-09-16 21:39:04 -0400

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,60 @@
 const path = require("path");
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const FixDefaultImportPlugin = require('webpack-fix-default-import-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { ContextReplacementPlugin } = require('webpack');
 
 module.exports = {
-    devtool: 'source-map',
-    context: __dirname,
-    entry: {
-        "kosher-zmanim": "./src/kosher-zmanim.ts",
-        "kosher-zmanim.min": "./src/kosher-zmanim.ts",
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "./dist"),
-        libraryTarget: "umd",
-        library: "KosherZmanim",
-        libraryExport: "default",
-        umdNamedDefine: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            transpileOnly: true,
-                            logInfoToStdOut: true,
-                            compilerOptions: {
-                                target: "es5",
-                                module: "es6",
-                            },
-                            configFile: "src/tsconfig.json",
-                        },
-                    },
-                ],
+  mode: "production",
+  devtool: 'source-map',
+  context: __dirname,
+  entry: {
+    "kosher-zmanim": "./src/kosher-zmanim.ts",
+    "kosher-zmanim.min": "./src/kosher-zmanim.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    libraryTarget: "umd",
+    library: "KosherZmanim",
+    libraryExport: "default",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              logInfoToStdOut: true,
+              compilerOptions: {
+                target: "es5",
+                module: "es6",
+              },
+              configFile: "src/tsconfig.json",
             },
+          },
         ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    plugins: [
-        new UglifyJSPlugin({
-                sourceMap: true,
-                include: /\.min\.js$/,
-                uglifyOptions: {
-                    mangle: {
-                        keep_fnames: true,
-                    },
-                },
-            },
-        ),
-        new FixDefaultImportPlugin(),
+      },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      sourceMap: true,
+      include: /\.min\.js$/,
+      uglifyOptions: {
+        mangle: {
+          keep_fnames: true,
+        },
+      },
+    })],
+  },
+  plugins: [
+    new ContextReplacementPlugin(/moment[/\\]locale$/, /en/),
+  ],
 };
 
