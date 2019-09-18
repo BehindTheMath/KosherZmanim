81b9f56e7de98077128f1ba449e0501cb4e4c208
Behind The Math <info@behindthemath.io>
2019-09-18 00:04:16 -0400

    Refactor the API structure

--- README.md
+++ README.md
@@ -20,12 +20,20 @@ npm install --save kosher-zmanim
 #### Importing
 **ES6 modules / Typescript:**
 ```javascript
-import KosherZmanim from "kosher-zmanim";
+import * as KosherZmanim from "kosher-zmanim";
+```
+Or:
+```javascript
+import { ComplexZmanimCalendar, getZmanimJson } from "kosher-zmanim";
 ```
 
-**Node:**
+**CommonJS modules:**
 ```javascript
 const KosherZmanim = require("kosher-zmanim");
+```
+Or:
+```javascript
+const { ComplexZmanimCalendar, getZmanimJson } = require("kosher-zmanim");
 ```
 
 **UMD (browser):**
@@ -33,17 +41,18 @@ const KosherZmanim = require("kosher-zmanim");
 For UMD, a global `KosherZmanim` object is exposed.
 
 #### Library Usage:
+The KosherJava library has been ported to JS, following the original API as close as possible. 
+The classes are exposed as named exports. You can instatiate or extend those classes as necessary, the same way you would in Java.
+
 ```javascript
-const zmanimCalendar = new KosherZmanim.lib.ZmanimCalendar();
+const zmanimCalendar = new KosherZmanim.ZmanimCalendar();
 ```
-The KosherJava library has been ported to JS, following the original API as close as possible. This is exposed as `KosherZmanim.lib.*`, following the above example. You can instatiate or extend those classes as necessary, the same way you would in Java.
 
 #### Simple usage
-Alternatively, KosherZmanim can be instatiated, and then there are 2 utility methods: `getZmanimJson()`, and `getZmanimXML()`.
+Alternatively, there is a `getZmanimJson()` utility method.
 
 ```javascript
-const kosherZmanim = new KosherZmanim(options);
-const zmanim = kosherZmanim.getZmanimJson();
+const zmanim = KosherZmanim.getZmanimJson(options);
 ```
 (See [here](/examples/frontend-example/frontend-example.html) for an example).
 
@@ -66,3 +75,4 @@ The zmanim are calculated based on the day of year, which is not dependent on ti
 
 # Breaking changes from KosherJava
 * `AstronomicalCalendar.getTemporalHour()` returns `null` instead of `Long.MIN_VALUE` if the calculations cannot be completed.
+* JS/TS does not have a parallel to Java's `Long.MIN_VALUE`, so `Long_MIN_VALUE` is set to `NaN`.
--- src/kosher-zmanim.ts
+++ src/kosher-zmanim.ts
@@ -1,81 +1,62 @@
-import {Time} from "./util/Time";
 import {GeoLocation} from "./util/GeoLocation";
-import {GeoLocationUtils} from "./util/GeoLocationUtils";
-
-import {NOAACalculator} from "./util/NOAACalculator";
-import {SunTimesCalculator} from "./util/SunTimesCalculator";
-
-import {AstronomicalCalendar} from "./AstronomicalCalendar";
 import {ZmanimCalendar} from "./ZmanimCalendar";
 import {ComplexZmanimCalendar} from "./ComplexZmanimCalendar";
+import {JsonOutput, ZmanimFormatter} from "./util/ZmanimFormatter";
 
-import {JewishDate} from "./hebrewcalendar/JewishDate";
-import {JewishCalendar} from "./hebrewcalendar/JewishCalendar";
-import {Daf} from "./hebrewcalendar/Daf";
-import {YomiCalculator} from "./hebrewcalendar/YomiCalculator";
-import {YerushalmiYomiCalculator} from "./hebrewcalendar/YerushalmiYomiCalculator";
+import * as _MomentTimezone from "moment-timezone";
+import Moment = _MomentTimezone.Moment;
 
-import {HebrewDateFormatter} from "./hebrewcalendar/HebrewDateFormatter";
-import {ZmanimFormatter, JsonOutput} from "./util/ZmanimFormatter";
-
-import * as MomentTimezone from "moment-timezone";
-import Moment = MomentTimezone.Moment;
-
-export class KosherZmanim {
-    private zmanimCalendar: ZmanimCalendar;
-
-    public static readonly lib = {
-        Time: Time,
-        GeoLocation: GeoLocation,
-        GeoLocationUtils: GeoLocationUtils,
-
-        NOAACalculator: NOAACalculator,
-        SunTimesCalculator: SunTimesCalculator,
-
-        AstronomicalCalendar: AstronomicalCalendar,
-        ZmanimCalendar: ZmanimCalendar,
-        ComplexZmanimCalendar: ComplexZmanimCalendar,
-
-        JewishDate: JewishDate,
-        JewishCalendar: JewishCalendar,
-        Daf: Daf,
-        YomiCalculator: YomiCalculator,
-        YerushalmiYomiCalculator: YerushalmiYomiCalculator,
-
-        HebrewDateFormatter: HebrewDateFormatter,
-        ZmanimFormatter: ZmanimFormatter,
-
-        MomentTimezone: MomentTimezone
-    };
-
-    constructor(options: ZmanimConstructorOptions) {
-        const moment: Moment = MomentTimezone.tz(options.date, options.timeZoneId);
-
-        const geoLocation: GeoLocation = new GeoLocation(options.locationName, options.latitude, options.longitude, options.elevation || 0);
-        geoLocation.setTimeZone(options.timeZoneId);
-
-        this.zmanimCalendar = options.complexZmanim ? new ComplexZmanimCalendar(geoLocation) : new ZmanimCalendar(geoLocation);
-        this.zmanimCalendar.setMoment(moment);
-    }
-
-    /**
-     * @deprecated
-     */
-    public getZmanimXML(): string {
-        throw new Error("This method is no longer supported");
-    }
-
-    public getZmanimJson(): JsonOutput {
-        return ZmanimFormatter.toJSON(this.zmanimCalendar);
-    }
+/**
+ * @deprecated
+ */
+export function getZmanimXML(): string {
+  throw new Error("This method is no longer supported");
 }
 
-export interface ZmanimConstructorOptions {
-    date: Date | string | number;
-    timeZoneId: string;
-    locationName: string;
-    latitude: number;
-    longitude: number;
-    elevation?: number;
-    complexZmanim?: boolean;
+export function getZmanimJson(options: Options): JsonOutput {
+  const moment: Moment = _MomentTimezone.tz(options.date, options.timeZoneId);
+
+  const geoLocation: GeoLocation = new GeoLocation(options.locationName, options.latitude, options.longitude,
+    options.elevation || 0);
+  geoLocation.setTimeZone(options.timeZoneId);
+
+  const zmanimCalendar: ZmanimCalendar = options.complexZmanim
+    ? new ComplexZmanimCalendar(geoLocation)
+    : new ZmanimCalendar(geoLocation);
+  zmanimCalendar.setMoment(moment);
+  return ZmanimFormatter.toJSON(zmanimCalendar);
 }
+
+export interface Options {
+  date: Date | string | number;
+  timeZoneId: string;
+  locationName: string;
+  latitude: number;
+  longitude: number;
+  elevation?: number;
+  complexZmanim?: boolean;
+}
+
+export * from "./util/Time";
+export * from "./util/GeoLocation";
+export * from "./util/GeoLocationUtils";
+export * from "./util/Zman";
+export * from "./polyfills/Utils";
+
+export * from "./util/NOAACalculator";
+export * from "./util/SunTimesCalculator";
+
+export * from "./AstronomicalCalendar";
+export * from "./ZmanimCalendar";
+export * from "./ComplexZmanimCalendar";
+
+export * from "./hebrewcalendar/JewishDate";
+export * from "./hebrewcalendar/JewishCalendar";
+export * from "./hebrewcalendar/Daf";
+export * from "./hebrewcalendar/YomiCalculator";
+export * from "./hebrewcalendar/YerushalmiYomiCalculator";
+
+export * from "./hebrewcalendar/HebrewDateFormatter";
+export * from "./util/ZmanimFormatter";
+
+export const MomentTimezone = _MomentTimezone;
